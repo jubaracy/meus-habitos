83170dca8f0535d89559923b134a3340eabf55a4
jubaracy <baracymktdigital@gmail.com>
2025-04-07 17:57:42 -0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,32 +1,52 @@
 // utils.js
 
-import Chart from 'chart.js/auto';
-
-export function formatarData(data) {
+function formatarData(data) {
   return data.toISOString().split('T')[0];
 }
 
-export function gerarGraficoMensal(dados) {
-  const dias = Object.keys(dados).sort();
-  const contagem = {};
+function obterDataAtual() {
+  return formatarData(new Date());
+}
 
-  dias.forEach(dia => {
-    dados[dia].forEach(h => {
-      if (!contagem[h.nome]) contagem[h.nome] = 0;
-      if (h.feito) contagem[h.nome]++;
+function gerarIdUnico() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function mostrarAlerta(mensagem) {
+  const alerta = document.createElement('div');
+  alerta.className = 'alerta';
+  alerta.textContent = mensagem;
+  document.body.prepend(alerta);
+  setTimeout(() => alerta.remove(), 4000);
+}
+
+function agruparPorMes(dados) {
+  const agrupado = {};
+  Object.entries(dados).forEach(([data, habitos]) => {
+    const [ano, mes] = data.split('-');
+    const chaveMes = `${ano}-${mes}`;
+    if (!agrupado[chaveMes]) agrupado[chaveMes] = [];
+    agrupado[chaveMes].push({ data, habitos });
+  });
+  return agrupado;
+}
+
+function contarFrequenciaMensal(historicoMensal, habitosBase) {
+  const frequencia = {};
+  habitosBase.forEach(h => frequencia[h.nome] = 0);
+  historicoMensal.forEach(({ habitos }) => {
+    habitos.forEach(h => {
+      if (h.feito && frequencia[h.nome] !== undefined) {
+        frequencia[h.nome]++;
+      }
     });
   });
+  return frequencia;
+}
 
-  const ctx = document.getElementById("graficoFrequencia");
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: Object.keys(contagem),
-      datasets: [{
-        label: "% de Conclusão no Mês",
-        data: Object.values(contagem),
-        backgroundColor: "#333"
-      }]
-    }
-  });
+function criarElemento(tag, classe, texto) {
+  const el = document.createElement(tag);
+  if (classe) el.className = classe;
+  if (texto) el.textContent = texto;
+  return el;
 }

864c6b364e99b536c5bfbfc0761706c95db5aac5
jubaracy <baracymktdigital@gmail.com>
2025-04-07 17:35:16 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
 
+function getHabits() {
+  return [
+    { nome: "Beber Água", categoria: "Saúde" },
+    { nome: "Academia", categoria: "Saúde" },
+    { nome: "Meditação", categoria: "Bem-estar" },
+    { nome: "Leitura", categoria: "Pessoal" },
+    { nome: "Entregar lixo", categoria: "Casa" },
+    { nome: "Limpar cozinha", categoria: "Casa" }
+  ];
+}
+

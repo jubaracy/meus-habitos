1fc6df51b3a4a92d4de8c1fea0c29907ffe32a6b
jubaracy <baracymktdigital@gmail.com>
2025-04-07 17:47:27 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,26 @@
 
-// data.js
-
-const STORAGE_KEY = 'meusHabitos';
-
 export function salvarDados(dados) {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dados));
+  localStorage.setItem("dados", JSON.stringify(dados));
 }
 
 export function carregarDados() {
-  const dados = localStorage.getItem(STORAGE_KEY);
-  return dados ? JSON.parse(dados) : null;
+  return JSON.parse(localStorage.getItem("dados")) || {};
 }
 
-export function limparDados() {
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-export function exportarDados() {
-  const dados = carregarDados();
+export function exportarComoJSON(dados) {
   const blob = new Blob([JSON.stringify(dados, null, 2)], { type: 'application/json' });
   const url = URL.createObjectURL(blob);
-  const a = document.createElement('a');
+  const a = document.createElement("a");
   a.href = url;
-  a.download = 'meus_habitos_backup.json';
+  a.download = "meus-habitos.json";
   a.click();
   URL.revokeObjectURL(url);
+}
+
+export function trocarSenhaStorage(novaSenha) {
+  localStorage.setItem("senha", novaSenha);
+}
+
+export function verificarSenha(senhaDigitada) {
+  return senhaDigitada === (localStorage.getItem("senha") || "1234");
 }
